a371da30a584708d3ed71ce1ac9ed5532e6cfa6d
Saman Gautam <samangautam443@gmail.com>
2024-03-05 18:10:48 +0545

    Bug fixed
    Fixed a bug where Week2Servlet was not being read

--- Week2Workshop/src/main/java/Week2Servlet.java
+++ Week2Workshop/src/main/java/Week2Servlet.java
@@ -14,16 +14,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class MyFirstServlet
+ * Servlet implementation class Week2Servlet
  */
-@WebServlet("/MyFirstServlet")
-public class MyFirstServlet extends HttpServlet {
+@WebServlet(asyncSupported = true, urlPatterns = { "/Week2Servlet" })
+public class Week2Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MyFirstServlet() {
+    public Week2Servlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,8 +31,7 @@ public class MyFirstServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter printOut = response.getWriter();
 		response.setContentType("text/html");
 		printOut.print("HelloWorld");
@@ -41,14 +40,16 @@ public class MyFirstServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 		response.setContentType("text/html");
 		PrintWriter printOut = response.getWriter();
-		
-		String username = request.getParameter("username");
-		String age = request.getParameter("age");
+
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+		String email = request.getParameter("email");
+		int phoneNumber = Integer.parseInt(request.getParameter("phoneNumber"));
+		String subject = request.getParameter("subject");
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -58,24 +59,36 @@ public class MyFirstServlet extends HttpServlet {
 			String pass = "";
 			Connection con = DriverManager.getConnection(url, user, pass);
 			
-			String query = "Insert into week2 (username, age) values (?,?)";
+			String query = "Insert into workshop2 (firstname, lastname, email, phoneNumber, subject) values (?,?,?,?,?)";
 			PreparedStatement st = con.prepareStatement(query);
 			
-			st.setString(1, username);
-			st.setString(2, age);
+			st.setString(1, firstname);
+			st.setString(2, lastname);
+			st.setString(3, email);
+			st.setInt(4, phoneNumber);
+			st.setString(5, subject);
+			
+			st.executeUpdate(); 
+			
+			PrintWriter out = response.getWriter();
+			out.println("<h1>Your account has been registered!!!</h1>");
+			out.println("<h3>Name:" + firstname + " " + lastname + "</h3>");
+			out.println("<h3>Email: " + email + "</h3>");
+			out.println("<h3>Phone Number: " + phoneNumber + "</h3>");
+			out.println("<h3>Subject: " + subject + "</h3>");
 			
 			int result = st.executeUpdate();
 			
 			if (result > 0) {
 				printOut.println("<h1>Your account is registered as </h1>");
-				printOut.println("<h3>Name:" +username+ "</h3>");
-				printOut.println("<h3>Age:" +age+ "</h3>");
+				printOut.println("<h3>Name:" +firstname+ "</h3>");
+				printOut.println("<h3>Age:" +lastname+ "</h3>");
 			}
 			else {
 				printOut.println("<h3>Sorry! Your data is not registered!!!</h3>");
 			}
 		}
-		catch (SQLException | ClassNotFoundException ex) {
+		catch(SQLException | ClassNotFoundException ex) {
 			printOut.println("<h1>Please enter the correct data!</h1>");
 		}
 	}
