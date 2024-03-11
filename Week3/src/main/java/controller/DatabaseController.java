4eaa7242e9561b94598b390731875ff1abe11150
Saman Gautam <samangautam443@gmail.com>
2024-03-11 18:53:54 +0545

    Week 3
    Login and Register pushed

---
+++ Week3/src/main/java/controller/DatabaseController.java
@@ -0,0 +1,72 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import util.StringUtils;
+import model.StudentModel;
+
+public class DatabaseController {
+//	public static final String GET_LOGIN_STUDENT_INFO = "SELECT user_name, password from student_info";
+	public Connection getConnection() throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+		String url = "jdbc:mysql://localhost:3306/student_management";
+		String user = "root";
+		String pass = "";
+		return DriverManager.getConnection(url, user, pass);
+	}
+	
+	public int addStudent(StudentModel studentModel) {
+		String INSERT_STUDENT = "INSERT INTO student_info" + "(first_name, last_name, birthday, gender, email, "
+				+ "number, subject, user_name, password)" + "VALUES (?,?,?,?,?,?,?,?,?)";
+		try(Connection con = getConnection()){
+			PreparedStatement st = con.prepareStatement(INSERT_STUDENT);
+			
+			st.setString(1, studentModel.getFirstName());
+			st.setString(2, studentModel.getLastName());
+			st.setDate(3, Date.valueOf(studentModel.getDob()));
+			st.setString(4, studentModel.getGender());
+			st.setString(5, studentModel.getEmail());
+			st.setString(6, studentModel.getPhoneNumber());
+			st.setString(7, studentModel.getSubject());
+			st.setString(8, studentModel.getUsername());
+			st.setString(10, studentModel.getPassword());
+			
+			int result = st.executeUpdate();
+			return result > 0 ? 1:0;
+		}
+		catch(SQLException | ClassNotFoundException ex) {
+			ex.printStackTrace(); //Log for exception for debugging
+			return -1;
+		}
+	}
+	
+	public int getStudentLoginInfo(String user_name, String password) {
+		try (Connection con = getConnection()){
+			PreparedStatement st = con.prepareStatement(StringUtils.GET_LOGIN_STUDENT_INFO);
+			
+			st.setString(1, user_name);
+			st.setString(2, password);
+			ResultSet rs = st.executeQuery();
+			
+			if(rs.next()) {
+				//Username and password matches to database
+				return 1;
+			}
+			else {
+				//No matching record
+				
+				return 0;
+			}
+		}
+		catch(SQLException | ClassNotFoundException ex) {
+			ex.printStackTrace(); //Log to send exception
+			
+			return -1;
+		}
+	}
+}
---
+++ Week3/src/main/java/model/StudentModel.java
@@ -0,0 +1,100 @@
+package model;
+
+import java.time.LocalDate;
+
+public class StudentModel {
+	private String firstName;
+	private String lastName;
+	private LocalDate dob;
+	private String gender;
+	private String email;
+	private String phoneNumber;
+	private String subject;
+	private String username;
+	private String password;
+	
+	public StudentModel (String firstName, String lastName, LocalDate dob, String gender, String email, String phoneNumber, String subject,
+			String username, String password) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.gender = gender;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.subject = subject;
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public LocalDate getDob() {
+		return this.dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public String getGender() {
+		return this.gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getSubject() {
+		return this.subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+}
