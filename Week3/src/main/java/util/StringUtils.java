cbdb4292c7dd184b51cf03f4f1c924407cecfb15
Saman Gautam <samangautam443@gmail.com>
2024-07-12 16:15:26 +0545

    Unnecessary Folders removed
    Pushed 7/12/2024

--- Week3/src/main/java/util/StringUtils.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class StringUtils {
-	public static final String GET_LOGIN_STUDENT_INFO = "SELECT user_name, password from student_info";
-	
-	public static final String GET_ALL_STUDENT_INFO = "SELECT * FROM student_info WHERE user_name = ? AND password = ?";
-	
-}
